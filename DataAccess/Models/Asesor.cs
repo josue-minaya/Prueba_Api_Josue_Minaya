bbeeb93fe4411c1e2255d2e9e810102dd4b0ec3f
jminaya <josue.minaya@upc.pe>
2023-02-12 22:53:10 -0500

    .

--- DataAccess/Models/Asesor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Prueba_Josue_Minaya.Models
-{
-    public class Asesor
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-
-    }
-}
--- DataAccess/Models/Producto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Prueba_Josue_Minaya.Models
-{
-    public class Producto
-    {
-        public int IdProducto { get; set; }
-        public double Precio { get; set; }
-        public string NOmbre { get; set; }
-    }
-}
